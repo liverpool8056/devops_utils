a24b92724d8c78d6af4921cfbd2510ea4d2e7d39
向文超 <robin@xiangwenchaos-MacBook-Pro.local>
2019-09-07 09:53:12 +0800

    compatible with windows, macos, centos
    optimize cmd argss
    
    add import rebase

--- python/ping.py
+++ python/ping.py
@@ -9,10 +9,26 @@ import sys
 import argparse
 import sys
 
+import rebase
+
 class PingTool:
 
+    PING_CMD_SET = {
+        'win32': 'ping {ip} -n {repeat} -w 200',
+        'darwin': 'ping {ip} -c {repeat} -w 200',
+        'linux': 'ping {ip} -c {repeat} -w 1'
+    }
+    PING_CMD = PING_CMD_SET[sys.platform]
+    UNREACHABLE_STRING_SET = {
+        'win32': '100% loss',
+        'darwin': '100.0% packet loss',
+        'linux': '100% packet loss'
+    }
+    UNREACHABLE_STRING = UNREACHABLE_STRING_SET[sys.platform]
+    repeat = 1
+    state = 'all'
     netStr = ''
-    pingCmd = 'ping {ip} -n {repeat} -w 200'
+
     IPs = None
     result = []
     __field_names = ['IP', 'isReachable']
@@ -25,48 +41,68 @@ Usage: python ping.py [-n] [-r count] [-n net]
 
 Options:
     -h,--help       Print usage
-    -r              Number of echo requests to send
+    -c              Count of ping requests to be sent
     -n              Network to test
+    -s              return a subset of ip list. True is for reachable ip, false is for unreachable 
 """
 
     def __init__(self):
-        self.printUsage()
+        self.parser_cli()
+        self.print_args()
         
         self.IPs = IP(self.netStr)
         self.__pt.field_names = self.__field_names
 
-    def printUsage(self):
+    def parser_cli(self):
         if len(sys.argv) < 2:
             print(self.__USAGE)
             sys.exit(0)
-        opts, args = getopt.getopt(sys.argv[1:],"n:r:h",["help"])
+        opts, args = getopt.getopt(sys.argv[1:],"n:c:s:h",["help"])
         for opt, arg in opts:
             if opt in ['-n']:
-                print('net:' + arg)
                 self.netStr = arg
-            elif "-r" == opt:
-                print('repeat:' + arg)
-                self.pingCmd = self.pingCmd.format(repeat=arg, ip='{ip}')
+            elif "-c" == opt:
+                self.repeat = arg
+                self.pingCmd = self.PING_CMD.format(repeat=arg, ip='{ip}')
+            elif "-s" == opt:
+                if arg == 'true':
+                    self.state = True
+                elif arg == 'false':
+                    self.state = False
+                else:
+                    print(self.__USAGE)
+                    sys.exit(0)
             elif opt in ["--help", "-h"]:
                 print(self.__USAGE)
                 sys.exit(0)
+
+    def print_args(self):
+        tmp = ''
+        tmp += 'count: %s\t'%self.repeat
+        tmp += 'network: %s\t'%self.netStr
+        tmp += 'state: %s'%self.state
+        print(tmp)
             
     def getIPs(self):
         print(self.IPs.len())
         for ip in self.IPs:
             print(ip)
 
-    def report(self):
+    def report(self, state='all'):
         for r in self.result:
             self.__pt.add_row(r)
         print(self.__pt)
 
-    def test(self):
+    def filter(self):
+        self.result = [ r for r in self.result if r[-1]==self.state ]
+        return self
+
+    def probe(self):
         self.result = []
         tmp_futures = []
         isFinish = False
         for ip in self.IPs:
-            tmp_futures.append(self.__executor.submit(os.popen, self.pingCmd.format(ip=ip, repeat=1)))
+            tmp_futures.append(self.__executor.submit(os.popen, self.pingCmd.format(ip=ip, repeat=self.repeat)))
         while (not isFinish):
             for f in tmp_futures:
                 if not f.done():
@@ -79,7 +115,7 @@ Options:
             self.result.append(
                 (
                     ip, 
-                    False if "100% loss" in tmp_futures[idx].result().read() else True
+                    False if self.UNREACHABLE_STRING in tmp_futures[idx].result().read() else True
                 )
             )
         return self
@@ -87,4 +123,4 @@ Options:
 if __name__ == "__main__":
     # pt = PingTool('172.20.232.0/24')
     pt = PingTool()
-    pt.test().report()
+    pt.probe().filter().report()
