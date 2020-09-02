735c72f370f5706ca61892c5083f02b102548250
robin <liverpool8056@163.com>
2020-09-02 15:40:25 +0800

    deviceService

--- python/find_incomplete_device.py
+++ python/find_incomplete_device.py
@@ -1,23 +1,27 @@
 from NetworkManagement.Service.DeviceService import NetworkDeviceService
 
-def get_incomplete_device(devices):
-    return [ d for d in devices if d.sysOid!= 'None' and d.model.find('No ')>= 0 and d.location=='sh' ]
+def get_incomplete_device(devices, location='sh'):
+    return [ d for d in devices if (d.sysOid!= 'None' or d.sysOid!='') and (d.model.find('No ')>= 0 or d.model=='None' or d.model=='') and d.location==location ]
 
 def get_sysOid_set(devices):
-    sysOid_set = set()
+    sysOid_dict = dict()
     for d in devices:
         if d.sysOid:
-            sysOid_set.add(d.sysOid)
-    return sysOid_set
+            if d.sysOid in sysOid_dict:
+                sysOid_dict[d.sysOid].append((d.managementIP, d.name))
+            else:
+                sysOid_dict[d.sysOid] = [(d.managementIP, d.name)]
+        print(sysOid_dict)
+    return sysOid_dict
 
 if __name__ == '__main__':
 
     ds = NetworkDeviceService()
     devices = ds.getAllDevices(known=False)
 
-    devices = get_incomplete_device(devices)
-    for d in devices:
-        print(d.items())
+    devices = get_incomplete_device(devices, location='sh')
+    #for d in devices:
+    #    print(d.items())
 
-    #sysOidSet = get_sysOid_set(devices)
-    #print(sysOidSet)
+    sysOidSet = get_sysOid_set(devices)
+    print(sysOidSet)
